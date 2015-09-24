0edaf24bddfa9401a6b941b047d24f66f3bc4aac
Ashish Kumar <ashish21099@gmail.com>
2015-09-24 15:25:17 +0530

    multidex

--- app/src/main/java/reach/project/core/ReachApplication.java
+++ app/src/main/java/reach/project/core/ReachApplication.java
@@ -2,6 +2,7 @@ package reach.project.core;
 
 import android.app.Application;
 import android.content.Context;
+import android.support.multidex.MultiDex;
 
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
@@ -21,6 +22,6 @@ public class ReachApplication extends Application {
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         ////meant for release
-        //MultiDex.install(this);
+        MultiDex.install(this);
     }
 }
