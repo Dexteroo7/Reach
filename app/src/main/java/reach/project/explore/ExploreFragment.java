431078f43d7333105d9590a628cdb925d2a6c9de
Ashish Kumar <ashish21099@gmail.com>
2015-11-15 16:38:02 +0530

    added explore api

--- app/src/main/java/reach/project/explore/ExploreFragment.java
+++ app/src/main/java/reach/project/explore/ExploreFragment.java
@@ -4,9 +4,19 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
@@ -98,40 +108,96 @@ public class ExploreFragment extends Fragment implements ExploreAdapter.Explore,
     }
 
     private static int counter = 0;
+
     private static final Callable<Collection<ExploreContainer>> fetchNextBatch = () -> {
 
-        try {
+        /*try {
             Thread.sleep(2000L);
         } catch (InterruptedException e) {
             e.printStackTrace();
             return null;
         }
-
-        /*final Cursor cursor = MiscUtils.useContextFromFragment(reference, context -> {
-            return context.getContentResolver().query(ReachSongProvider.CONTENT_URI,
-                    new String[]{ReachSongHelper.COLUMN_DISPLAY_NAME},
-                    null, null, null);
-        }).orNull();
-
-        if (cursor == null)
-            return null;*/
-
         final List<ExploreContainer> containers = new ArrayList<>(10);
         for (int i=0; i<20; i++)
             containers.add(new ExploreContainer("Page " + i, "Subtitle", "imageId", "userImageId",
                     "handle", 3.0f, ExploreTypes.MUSIC, new Random().nextLong()));
-        /*while (cursor.moveToNext())
-            containers.add(new ExploreContainer(cursor.getString(0), ExploreTypes.MUSIC));*/
-
         counter += containers.size();
         if (counter > 50) {
-
-//            MiscUtils.runOnUiThreadFragment(reference, context -> {
-//                Toast.makeText(context, "Server sending done for today", Toast.LENGTH_SHORT).show();
-//            });
-
             containers.clear();
             containers.add(new ExploreContainer(ExploreTypes.DONE_FOR_TODAY, new Random().nextLong()));
+        }*/
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("userId", "6571391281266688");
+        JSONArray jsonArray = new JSONArray();
+        for (int i=0; i<10; i++) {
+            JSONObject jsonObject1 = new JSONObject();
+            jsonObject1.put("friendId", "4503618472378368");
+            jsonArray.put(jsonObject1);
+        }
+        /*JSONObject jsonObject1 = new JSONObject();
+        jsonObject1.put("friendId", "4503618472378368");
+        jsonArray.put(jsonObject1);
+        JSONObject jsonObject2 = new JSONObject();
+        jsonObject2.put("friendId", "4505211267710976");
+        jsonArray.put(jsonObject2);
+        JSONObject jsonObject3 = new JSONObject();
+        jsonObject3.put("friendId", "4503788425576448");
+        jsonArray.put(jsonObject3);
+        JSONObject jsonObject4 = new JSONObject();
+        jsonObject4.put("friendId", "4504653291061248");
+        jsonArray.put(jsonObject4);
+        JSONObject jsonObject5 = new JSONObject();
+        jsonObject5.put("friendId", "4504219130265600");
+        jsonArray.put(jsonObject5);*/
+
+        jsonObject.put("friends", jsonArray);
+        Log.d("Ashish", jsonObject.toString());
+
+        /*HttpURLConnection con = (HttpURLConnection) new URL("http://52.74.175.56:8080/explore/getObjects").openConnection();
+        con.setRequestMethod("POST");
+        //con.setDoInput(true);
+        con.setDoOutput(true);
+        con.connect();
+
+        OutputStream os = con.getOutputStream();
+        BufferedWriter writer = new BufferedWriter(
+                new OutputStreamWriter(os));
+        writer.write(jsonObject.toString());
+        writer.close();
+        os.close();
+
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String line;
+        StringBuilder sb = new StringBuilder();
+        while ((line = br.readLine()) != null)
+            sb.append(line);
+        br.close();*/
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody
+                .create(MediaType.parse("application/json; charset=utf-8"), jsonObject.toString());
+        Request request = new Request.Builder()
+                .url("http://52.74.175.56:8080/explore/getObjects")
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+
+        Log.d("Ashish", response.body().string());
+        JSONArray receivedData = new JSONArray(response.body().string());
+        final List<ExploreContainer> containers = new ArrayList<>();
+        for (int i=0; i<receivedData.length(); i++) {
+            JSONObject object = receivedData.getJSONObject(i);
+            String title = object.getString("displayName");
+            String subTitle = object.getString("artistName");
+            String imageId = object.getString("largeImageUrl");
+            long contentId = object.getLong("contentId");
+            int contentType = object.getInt("contentType");
+            long userId = object.getLong("userId");
+
+            containers.add(new ExploreContainer(title, subTitle, imageId, "userImageId",
+                    "handle", 3.0f, ExploreTypes.MUSIC, contentId));
         }
 
         return containers;
