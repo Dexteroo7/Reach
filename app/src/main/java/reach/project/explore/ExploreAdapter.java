acd6f28bb689752c9c71d812a4584f9a083293a2
Ashish Kumar <ashish21099@gmail.com>
2015-11-17 23:05:05 +0530

    explore UI finishing

--- app/src/main/java/reach/project/explore/ExploreAdapter.java
+++ app/src/main/java/reach/project/explore/ExploreAdapter.java
@@ -1,15 +1,23 @@
 package reach.project.explore;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
+import android.net.Uri;
 import android.support.v4.view.PagerAdapter;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.facebook.drawee.view.SimpleDraweeView;
+
 import reach.project.R;
+import reach.project.core.StaticData;
+import reach.project.utils.MiscUtils;
 
 /**
  * Created by dexter on 16/10/15.
@@ -24,6 +32,18 @@ public class ExploreAdapter extends PagerAdapter {
         this.explore = explore;
     }
 
+    private View.OnClickListener clickListener = v -> {
+        //long id = (long) v.getTag();
+        ValueAnimator animator = ValueAnimator.ofInt(0,  MiscUtils.dpToPx(5));
+        animator.setDuration(300);
+        animator.addUpdateListener(animation -> {
+            int val = (int) animation.getAnimatedValue();
+            v.setPadding(val, val, val, val);
+        });
+        animator.setInterpolator(new AccelerateInterpolator());
+        animator.start();
+    };
+
     @Override
     public Object instantiateItem(ViewGroup collection, int position) {
 
@@ -34,8 +54,9 @@ public class ExploreAdapter extends PagerAdapter {
         final TextView subTitle = (TextView) layout.findViewById(R.id.subtitle);
         final TextView userHandle = (TextView) layout.findViewById(R.id.userHandle);
         final TextView typeText = (TextView) layout.findViewById(R.id.typeText);
-        final ImageView image = (ImageView) layout.findViewById(R.id.image);
-        final ImageView userImage = (ImageView) layout.findViewById(R.id.userImage);
+        final SimpleDraweeView image = (SimpleDraweeView) layout.findViewById(R.id.image);
+        final SimpleDraweeView userImage = (SimpleDraweeView) layout.findViewById(R.id.userImage);
+        final ImageView downButton = (ImageView) layout.findViewById(R.id.downButton);
 
         switch (container.getTypes()) {
             case MUSIC:
@@ -43,8 +64,12 @@ public class ExploreAdapter extends PagerAdapter {
                 subTitle.setText(container.getSubTitle());
                 userHandle.setText(container.getUserHandle());
                 typeText.setText(container.getTypes().getTitle());
-                container.getImageId();
-                container.getUserImageId();
+                String imageId = container.getImageId();
+                if (imageId!=null && !TextUtils.isEmpty(imageId))
+                    image.setImageURI(Uri.parse(imageId));
+                userImage.setImageURI(Uri.parse(StaticData.cloudStorageImageBaseUrl + container.getUserImageId()));
+                //downButton.setTag(container.getId());
+                downButton.setOnClickListener(clickListener);
                 layout.setTag(POSITION_UNCHANGED);
                 break;
             case APP:
