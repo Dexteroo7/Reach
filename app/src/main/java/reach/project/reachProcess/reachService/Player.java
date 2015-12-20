4858e0faabd513393f76add952668278a85d8f19
Aman Mahajan <amanmj95@gmail.com>
2015-12-20 22:38:30 +0530

    debug

--- app/src/main/java/reach/project/reachProcess/reachService/Player.java
+++ app/src/main/java/reach/project/reachProcess/reachService/Player.java
@@ -1,6 +1,5 @@
 package reach.project.reachProcess.reachService;
 
-
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.media.AudioFormat;
@@ -21,6 +20,9 @@ import com.google.common.base.Optional;
 import com.google.common.base.Throwables;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.concurrent.CancellationException;
@@ -32,6 +34,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import reach.project.R;
 import reach.project.core.StaticData;
 import reach.project.reachProcess.decoder.BitStream;
 import reach.project.reachProcess.decoder.BitStreamException;
@@ -156,6 +159,32 @@ public class Player {
 
         if (whichPlayer == WhichPlayer.MediaPlayer)
             mediaPlayer.start();
+        else if(whichPlayer==WhichPlayer.Exoplayer)
+        {
+            mediaPlayer.start();
+ //           exoplayer.setPlayWhenReady(true);
+            //Uri uri=Uri.parse("http://dl.enjoypur.vc/upload_file/5570/6757/%20Bollywood%20Hindi%20Mp3%20Songs%202015/Hate%20Story%203%20(2015)%20Mp3%20Songs/01%20Tumhe%20Apna%20Banane%20Ka%20%28Hate%20Story%203%29%20Armaan%20Malik%20190Kbps.mp3");
+            //String path="android.resource://" + handlerInterface.getContext().getPackageName() + File.separator + R.raw.song;
+            //Uri uri=Uri.parse(path);
+            //File file=new File(path);
+            //FileInputStream inputStream;
+            //FileDescriptor fd=null;
+            //try {
+              //  inputStream = new FileInputStream(file);
+               // fd=inputStream.getFD();
+
+            //} catch (IOException e) {
+                //e.printStackTrace();
+            //}
+            //SampleSource sampleSource=new FrameworkSampleSource(handlerInterface.getContext(),uri,null);
+            /*SampleSource sampleSource=new FrameworkSampleSource(fd,0,file.length());
+            TrackRenderer audio=new MediaCodecAudioTrackRenderer(sampleSource,null,true);
+            ExoPlayer player= ExoPlayer.Factory.newInstance(1);
+            player.prepare(audio);
+            player.setPlayWhenReady(true);*/
+
+
+        }
         else {
             audioTrack.play();
             pauseDecoding.set(false);
@@ -256,18 +285,37 @@ public class Player {
     protected int createMediaPlayerIfNeeded(MediaPlayer.OnCompletionListener completionListener,
                                             MediaPlayer.OnErrorListener onErrorListener,
                                             String path) throws IOException, InterruptedException {
-        reset(); //throws InterruptedException
+        //reset(); //throws InterruptedException
         if(Build.VERSION.SDK_INT>=16)
         {
             whichPlayer=WhichPlayer.Exoplayer;
             if(exoplayer==null)
             {
-                exoplayer=ExoPlayer.Factory.newInstance(1);
+
+
             }
-            SampleSource sampleSource=new FrameworkSampleSource(handlerInterface.getContext(),Uri.parse(path),null);
+
+            File file=new File(path);
+            FileInputStream inputStream = new FileInputStream(file);
+            FileDescriptor fd=inputStream.getFD();
+            //SampleSource sampleSource=new FrameworkSampleSource(handlerInterface.getContext(), Uri.parse(path),null);
+            SampleSource sampleSource=new FrameworkSampleSource(fd,0,file.length());
             TrackRenderer audio=new MediaCodecAudioTrackRenderer(sampleSource,null,true);
+            exoplayer=ExoPlayer.Factory.newInstance(1);
             exoplayer.prepare(audio);
-            return (int)exoplayer.getDuration();
+            //exoplayer.setPlayWhenReady(true);
+            //return 205000000;
+            //return (int)exoplayer.getDuration();
+            if (mediaPlayer == null)
+            {
+                mediaPlayer = new MediaPlayer();
+                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                mediaPlayer.setOnCompletionListener(completionListener);
+                mediaPlayer.setOnErrorListener(onErrorListener);
+            }
+            mediaPlayer.setDataSource(path);
+            mediaPlayer.prepare();
+            return mediaPlayer.getDuration();
         }
         else
         {
@@ -289,77 +337,86 @@ public class Player {
 
 //        Log.i("Downloader", "Creating audio track");
         reset(); //throws InterruptedException
-        whichPlayer = WhichPlayer.AudioTrack;
+        //if(Build.VERSION.SDK_INT>=16)
+        {
+          //  whichPlayer=WhichPlayer.Exoplayer;
+           // return 1;
 
-        if (!path.isPresent() || TextUtils.isEmpty(path.get()) || path.get().equals("hello_world"))
-            throw new IOException("Given path is invalid");
+        }
+        //else
+        {
+            whichPlayer = WhichPlayer.AudioTrack;
 
-        //create source
-        playerSource = new PlayerSource(
-                handlerInterface,
-                path.get(),
-                contentLength);
+            if (!path.isPresent() || TextUtils.isEmpty(path.get()) || path.get().equals("hello_world"))
+                throw new IOException("Given path is invalid");
 
-        //start thread
-        pipeFuture = decoderService.submit(playerSource);
+            //create source
+            playerSource = new PlayerSource(
+                    handlerInterface,
+                    path.get(),
+                    contentLength);
 
-        final BitStream bitStream = new BitStream(playerSource.getSource(), StaticData.PLAYER_BUFFER_DEFAULT);
-        final Header frameHeader;
+            //start thread
+            pipeFuture = decoderService.submit(playerSource);
 
-        //do like this to prevent blocking
-        final Future<Header> getHeader = decoderService.submit(bitStream::readFrame);
+            final BitStream bitStream = new BitStream(playerSource.getSource(), StaticData.PLAYER_BUFFER_DEFAULT);
+            final Header frameHeader;
 
-        try {
-            //caller should handle InterruptedException
-            frameHeader = getHeader.get(5, TimeUnit.SECONDS);
-        } catch (ExecutionException | TimeoutException e) {
+            //do like this to prevent blocking
+            final Future<Header> getHeader = decoderService.submit(bitStream::readFrame);
 
             try {
-                bitStream.close();
-            } catch (BitStreamException ignored) {
-            }
+                //caller should handle InterruptedException
+                frameHeader = getHeader.get(5, TimeUnit.SECONDS);
+            } catch (ExecutionException | TimeoutException e) {
+
+                try {
+                    bitStream.close();
+                } catch (BitStreamException ignored) {
+                }
 //            playerSource.close();
 //            pipeFuture.cancel(true);
-            e.printStackTrace();
-            throw new IOException("Probably corrupt file : header fetch timed out, " + Throwables.getStackTraceAsString(e));
-        } finally {
-            getHeader.cancel(true);
+                e.printStackTrace();
+                throw new IOException("Probably corrupt file : header fetch timed out, " + Throwables.getStackTraceAsString(e));
+            } finally {
+                getHeader.cancel(true);
+            }
+
+            if (frameHeader == null)
+                throw new IOException("Probably corrupt file : null frameHeader");
+
+            final long duration = frameHeader.total_ms(contentLength);
+            final int sampleFrequency = frameHeader.frequency();
+            final boolean mono = frameHeader.mode() == Header.SINGLE_CHANNEL;
+            final int mode = mono ? AudioFormat.CHANNEL_OUT_MONO : AudioFormat.CHANNEL_OUT_STEREO;
+            final boolean createNew = audioTrack == null || audioTrack.getSampleRate() != sampleFrequency || audioTrack.getChannelCount() != (mono ? 1 : 2);
+
+            Log.i("Downloader", "Maximum MS " + duration);
+            Log.i("Downloader", "MS Per frame" + frameHeader.ms_per_frame() + " " + contentLength);
+            Log.i("Downloader", "Sample Frequency " + sampleFrequency);
+            Log.i("Downloader", "Mono " + mono);
+            Log.i("Downloader", "Create New " + createNew);
+            final int bufferSize = sampleFrequency * 2;
+
+            if (createNew) {
+                audioTrack = new AudioTrack(
+                        AudioManager.STREAM_MUSIC,
+                        sampleFrequency,
+                        mode,
+                        AudioFormat.ENCODING_PCM_16BIT,
+                        bufferSize,
+                        AudioTrack.MODE_STREAM);
+            }
+
+            //half fill with zeroes
+            final byte[] zeroes = new byte[bufferSize / 2];
+            Arrays.fill(zeroes, 0, zeroes.length, (byte) 0);
+            audioTrack.write(zeroes, 0, zeroes.length);
+
+            decodeFuture = decoderService.submit(new Decode(bitStream, frameHeader, mono, bufferSize / 2));
+
+            return duration;
         }
-
-        if (frameHeader == null)
-            throw new IOException("Probably corrupt file : null frameHeader");
-
-        final long duration = frameHeader.total_ms(contentLength);
-        final int sampleFrequency = frameHeader.frequency();
-        final boolean mono = frameHeader.mode() == Header.SINGLE_CHANNEL;
-        final int mode = mono ? AudioFormat.CHANNEL_OUT_MONO : AudioFormat.CHANNEL_OUT_STEREO;
-        final boolean createNew = audioTrack == null || audioTrack.getSampleRate() != sampleFrequency || audioTrack.getChannelCount() != (mono ? 1 : 2);
-
-        Log.i("Downloader", "Maximum MS " + duration);
-        Log.i("Downloader", "MS Per frame" + frameHeader.ms_per_frame() + " " + contentLength);
-        Log.i("Downloader", "Sample Frequency " + sampleFrequency);
-        Log.i("Downloader", "Mono " + mono);
-        Log.i("Downloader", "Create New " + createNew);
-        final int bufferSize = sampleFrequency * 2;
-
-        if (createNew) {
-            audioTrack = new AudioTrack(
-                    AudioManager.STREAM_MUSIC,
-                    sampleFrequency,
-                    mode,
-                    AudioFormat.ENCODING_PCM_16BIT,
-                    bufferSize,
-                    AudioTrack.MODE_STREAM);
-        }
-
-        //half fill with zeroes
-        final byte[] zeroes = new byte[bufferSize / 2];
-        Arrays.fill(zeroes, 0, zeroes.length, (byte) 0);
-        audioTrack.write(zeroes, 0, zeroes.length);
-
-        decodeFuture = decoderService.submit(new Decode(bitStream, frameHeader, mono, bufferSize / 2));
-
-        return duration;
     }
 
     private final class Decode implements Runnable {
