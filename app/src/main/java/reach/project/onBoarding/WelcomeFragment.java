a729c1ee7a4b1a0dfed9ae167369cea8912641c4
Ashish Kumar <ashish21099@gmail.com>
2015-12-23 16:39:02 +0530

    WIP - coach marks

--- app/src/main/java/reach/project/onBoarding/WelcomeFragment.java
+++ app/src/main/java/reach/project/onBoarding/WelcomeFragment.java
@@ -11,6 +11,8 @@ import android.widget.Button;
 import java.lang.ref.WeakReference;
 
 import reach.project.R;
+import tourguide.tourguide.Overlay;
+import tourguide.tourguide.TourGuide;
 
 public class WelcomeFragment extends Fragment {
 
@@ -30,6 +32,11 @@ public class WelcomeFragment extends Fragment {
 
         final View rootView = inflater.inflate(R.layout.fragment_welcome, container, false);
         Button getStartedBtn = (Button) rootView.findViewById(R.id.getStartedBtn);
+        TourGuide.init(getActivity()).with(TourGuide.Technique.Click)
+                .setPointer(null)
+                .setToolTip(null)
+                .setOverlay(new Overlay().setStyle(Overlay.Style.Rectangle))
+                .playOn(getStartedBtn);
         getStartedBtn.setOnClickListener(v -> mListener.onOpenNumberVerification());
         return rootView;
     }
