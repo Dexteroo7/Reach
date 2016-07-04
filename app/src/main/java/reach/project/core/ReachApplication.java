b7b71c4d90d70199c15818d885b6f80d15a76505
Ashish Kumar <ashish21099@gmail.com>
2016-07-04 16:52:25 +0530

    Reboot from v1.1.3

--- app/src/main/java/reach/project/core/ReachApplication.java
+++ app/src/main/java/reach/project/core/ReachApplication.java
@@ -1,10 +1,10 @@
 package reach.project.core;
 
-import android.app.Application;
 import android.graphics.Bitmap;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.multidex.MultiDexApplication;
 
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
@@ -38,7 +38,7 @@ import reach.project.R;
 /**
  * Created by ashish on 23/3/15.
  */
-public class ReachApplication extends Application implements MemoryTrimmableRegistry {
+public class ReachApplication extends MultiDexApplication implements MemoryTrimmableRegistry {
 
     public static OkHttpClient OK_HTTP_CLIENT;
 
